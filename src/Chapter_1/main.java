7f1f80407aa5ebfe345a919f9686b607cf919be2
EnverJ <Enlem1987@gmail.com>
2022-11-27 11:31:45 -0700

    Enum

---
+++ src/Chapter_1/main.java
@@ -0,0 +1,19 @@
+package Chapter_1;
+
+public class main {
+    public static void main(String[] args) {
+        Level ll= Level.South;
+        switch (ll){
+            case North:
+                System.out.println("This is North");
+            case East:
+                System.out.println("This is South");
+            case West:
+                System.out.println("This is East");
+            case South:
+                System.out.println("This is South");
+        }
+
+    }
+
+}
