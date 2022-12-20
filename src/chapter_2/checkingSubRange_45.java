e9d3ac56622df62a26b25131cafd1fdde1e1a29a
EnverJ <Enlem1987@gmail.com>
2022-12-19 20:04:24 -0700

    range from 0 to length

---
+++ src/chapter_2/checkingSubRange_45.java
@@ -0,0 +1,27 @@
+package chapter_2;
+
+import java.util.Objects;
+
+public class checkingSubRange_45 {
+    private static final int N_UPPER_BOUND = 101;
+    private final int n;
+
+
+    public checkingSubRange_45(int n) {
+        this.n = Objects.checkIndex(n,N_UPPER_BOUND);
+    }
+    public int yMinusX (int x, int y) {
+        Objects.checkFromToIndex(x,y,n);
+        // check weather the given subrange[give start, given end] is within the bounds of the range
+        // from [0, given length]. So, this method can be applied to the yMinusX() method to check that
+        // the range bounded by x and y, [x,y] is subrange of [0,n].
+        return y - x;
+    }
+
+    public static void main(String[] args) {
+        checkingSubRange_45 ch = new checkingSubRange_45(50);
+        int r = ch.yMinusX(1,50);
+        System.out.println(r);
+    }
+
+}
