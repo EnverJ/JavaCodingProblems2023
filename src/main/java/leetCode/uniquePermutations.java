ab65aa903a00886bc789f2314f9e8905d9025961
EnverJ <Enlem1987@gmail.com>
2023-01-24 20:59:16 -0700

    num

--- src/main/java/leetCode/uniquePermutations.java
+++ src/main/java/leetCode/uniquePermutations.java
@@ -1,6 +1,16 @@
 package leetCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class uniquePermutations {
     // Given a collection of numbers that might contain duplicates, return all possible permutations.
+    public List<Integer> premutation(){
+    List<Integer> num= new ArrayList<>();
+    for(int i =0; i<10; i++){
+        num.add(i);
+        }
+    return num;
+    }
 
 }
