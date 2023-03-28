3b5b97a104c4d7c6491ab6ebf9990b35b335a64b
EnverJ <Enlem1987@gmail.com>
2023-03-27 22:13:57 -0600

    String format

--- src/main/java/lambda/ConInt.java
+++ src/main/java/lambda/ConInt.java
@@ -1,2 +1,22 @@
-package lambda;public class ConInt {
-}
+package lambda;
+
+import org.testng.annotations.Test;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class ConInt {
+    // consumer Interface
+    @Test
+    public void Consumerint () {
+    ArrayList<Integer> arr = new ArrayList<Integer>();
+    arr.add(10);
+    arr.add(9);
+    arr.add(8);
+    arr.add(7);
+    arr.add(6);
+    Consumer<Integer> method = (n) -> { System.out.println(n); };
+    arr.forEach(method);
+    }
+
+   }
+
---
+++ src/main/java/lambda/singleMethod.java
@@ -0,0 +1,21 @@
+package lambda;
+
+public class singleMethod implements StringFunction {
+
+
+    @Override
+    public String run(String str) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        StringFunction exclaim = (s) -> s + ("!");
+        StringFunction ask = (s) -> s+ ("?");
+        printFormatted ("Hello", exclaim);
+        printFormatted("Hello", ask);
+    }
+    public static void printFormatted(String str, StringFunction format) {
+      String result = format.run(str);
+        System.out.println(result);
+    }
+}
