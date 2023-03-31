d3df712249594142674a69215730f38a58e4a801
EnverJ <Enlem1987@gmail.com>
2023-03-30 22:26:11 -0600

    thread

---
+++ src/main/java/lambda/threads.java
@@ -0,0 +1,18 @@
+package lambda;
+
+import org.testng.annotations.Test;
+
+public class threads implements Runnable {
+    @Test
+    @Override
+    public void run() {
+        System.out.println("This code is inside of the thread");
+    }
+    @Test
+    public  void main () {
+        threads th = new threads();
+     Thread thread =  new Thread(th);
+     thread.start();
+        System.out.println("This code is outside eof the thread");
+    }
+}
