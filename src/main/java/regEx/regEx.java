251ed4c58f58175673ed0643f1f294c75609fa8f
EnverJ <Enlem1987@gmail.com>
2023-04-02 22:50:02 -0600

    regex

--- src/main/java/regEx/regEx.java
+++ src/main/java/regEx/regEx.java
@@ -14,10 +14,9 @@ public class regEx {
     public void findTheMatch() {
         String match_found = "Match Found";
         String match_not_found = "Match Not Found";
-    Pattern pattern = Pattern.compile("EZO4School", Pattern.CASE_INSENSITIVE);
-    Matcher matcher = pattern.matcher("Ezo4School is oen of the best programming school");
-    boolean matchFound = matcher.find();
-        System.out.println(matchFound?match_found:match_not_found);
+        Pattern pattern = Pattern.compile("EZO4School", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher("Ezo4School is oen of the best programming school");
+        boolean matchFound = matcher.find();
+        System.out.println(matchFound ? match_found : match_not_found);
     }
-
 }
