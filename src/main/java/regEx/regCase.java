225dbdeb726d515f6dfb1f462be1aa689545e9a3
EnverJ <Enlem1987@gmail.com>
2023-04-09 14:28:15 -0600

    delimiter

--- src/main/java/regEx/regCase.java
+++ src/main/java/regEx/regCase.java
@@ -15,6 +15,20 @@ public class regCase {
             System.out.println("Pattern Found in " + m.start() + " to " + (m.end() - 1));
         }
     }
-}
+
 //Pattern Found in 0 to 1
 //Pattern Found in 5 to 6
+    @Test
+    public void regSplit () {
+        String text = "I1Parked2My3Car4In5My6Garage7.";
+        String delimiter = "\\d";
+        Pattern pattern = Pattern.compile(delimiter, Pattern.CASE_INSENSITIVE);
+        String[] result = pattern.split(text);
+        //iterating using foreach loop
+        for (String tem: result
+             ) {
+            System.out.println(tem);
+        }
+
+    }
+}
