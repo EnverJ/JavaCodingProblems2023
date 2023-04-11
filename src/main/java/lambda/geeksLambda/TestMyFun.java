42a28333940bc356b20872be126db81896660a22
EnverJ <Enlem1987@gmail.com>
2023-04-10 22:14:49 -0600

    geeks lambda

---
+++ src/main/java/lambda/geeksLambda/TestMyFun.java
@@ -0,0 +1,14 @@
+package lambda.geeksLambda;
+
+import org.testng.annotations.Test;
+
+public class TestMyFun {
+    @Test
+     public void testFun() {
+         funInterFace fun = (int x)-> System.out.println(2*x);
+         fun.abstractFun(5);
+         fun.normalFun();
+     }
+}
+//10
+//Hello
